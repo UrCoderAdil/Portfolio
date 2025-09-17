0fed4a0400d71cf1347b92c1e84af86825e268f9
Adil Umer <AdilUmer2005@gmail.com>
2025-09-17 09:33:38 +0500

    Fix TransitionProvider children nesting issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TransitionProvider children={children}/>
+        <TransitionProvider>
+  {children}
+</TransitionProvider>
+
      
       </body>
     </html>
